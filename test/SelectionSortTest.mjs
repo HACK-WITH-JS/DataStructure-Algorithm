506816107622a483e4aab87720232156452e663b
HACK-WITH-JS <jungmini2040@gmail.com>
2023-12-30 11:09:58 +0900

    add: 선택 정렬

---
+++ test/SelectionSortTest.mjs
@@ -0,0 +1,11 @@
+import { SelectionSort } from "../SelectionSort.mjs";
+
+let arr = [4,2,1,3];
+
+console.log("===== 정렬 전 =====");
+console.log(arr);
+
+SelectionSort(arr);
+
+console.log("===== 정렬 후 =====");
+console.log(arr);
